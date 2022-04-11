be23964877262b93e4cde6c7c8e1fc6010891d08
navsi100rom <navsi100rom@users.noreply.github.com>
2022-04-11 22:59:16 +0300

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+// FOR LATER
+
+
+
+
+// $(".line-transparent").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function(){
+//     $(".lines").removeClass("animated")  
+//   })
+  
+//   $(".line-transparent").hover(function(){
+//     $(".lines").addClass("animated");        
+//   })
+
+// $(".contact").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function(){
+//     $(".contact").removeClass("appear")  
+//   })
+  
+//   $(".contact").hover(function(){
+//     $(".contact").addClass("appear");        
+//   })
+
+
+//   $(".box").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function(){
+//     $(this).removeClass("animate")  
+//   })
+  
+//   $(".box").hover(function(){
+//     $(this).addClass("animate");        
+//   })
+
+//   document.getElementById("line-1-animate").style.backgroundColor = "green";
+
+//   document.getElementById("boxie").style.backgroundColor = "green";
