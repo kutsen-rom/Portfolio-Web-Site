c3ceed7ddedd372c844088b0522656082297c17b
navsi100rom <navsi100rom@users.noreply.github.com>
2022-04-15 15:59:14 +0300

    Add contact page

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,126 @@
 const projectPic = [document.getElementById('pr-1'), document.getElementById('pr-2'), document.getElementById('pr-3'), document.getElementById('pr-4'),document.getElementById('pr-5')];
-const projectName = [document.getElementById("name-1"), document.getElementById("name-2"), document.getElementById("name-3"), document.getElementById("name-4"), document.getElementById("name-5")]
-const projectDesc = [document.getElementById("desc-1"), document.getElementById("desc-2"), document.getElementById("desc-3"), document.getElementById("desc-4"), document.getElementById("desc-5")]
-const links = [document.getElementById("link-1"), document.getElementById("link-2"), document.getElementById("link-3"), document.getElementById("link-4"), document.getElementById("link-5")]
+const projectName = [document.getElementById("name-1"), document.getElementById("name-2"), document.getElementById("name-3"), document.getElementById("name-4"), document.getElementById("name-5")];
+const projectDesc = [document.getElementById("desc-1"), document.getElementById("desc-2"), document.getElementById("desc-3"), document.getElementById("desc-4"), document.getElementById("desc-5")];
+const links = [document.getElementById("link-1"), document.getElementById("link-2"), document.getElementById("link-3"), document.getElementById("link-4"), document.getElementById("link-5")];
 const show = " current-project-image corner-box";
 const showName = " curr-name";
 const showDesc = " curr-description"
-const hide = " hide"
+const hide = " hide";
 const previous = document.getElementById("left-arrow");
 const next = document.getElementById("right-arrow")
 const makePrevious = " previous-project";
 const makeNext = " next-project";
-let i=0;
+const skillsButton = document.getElementById("skills-button-main");
+const skillsPanel = document.getElementById("skills-panel");
+const animationSkills = "black"
+let elementNum=0;
+
 
 
 
 function showNext () {
-    links[i].className += hide;
-    projectName[i].className += hide;
-    projectDesc[i].className += hide;
-    projectPic[i].className -= show;
-    i += 1 
-    if (i > projectPic.length-1) {
-        i -= projectPic.length
+    links[elementNum].className += hide;
+    projectName[elementNum].className += hide;
+    projectDesc[elementNum].className += hide;
+    projectPic[elementNum].className -= show;
+    elementNum += 1 
+    if (elementNum > projectPic.length-1) {
+        elementNum -= projectPic.length
         
     
     }
-    links[i].className = -+ hide;
-    projectPic[i].className += show;
-    projectName[i].className -= hide;
-    projectDesc[i].className -= hide;
-    projectName[i].className += showName;
-    projectDesc[i].className += showDesc;
+    links[elementNum].className = -+ hide;
+    projectPic[elementNum].className += show;
+    projectName[elementNum].className -= hide;
+    projectDesc[elementNum].className -= hide;
+    projectName[elementNum].className += showName;
+    projectDesc[elementNum].className += showDesc;
     
    
 }
 
 function showPrevious () {
-    links[i].className += hide;
-    projectName[i].className += hide;
-    projectDesc[i].className += hide;
-    projectPic[i].className -= show;
-    i -= 1 
-    if (i<0) {
-        i += projectPic.length;
+    links[elementNum].className += hide;
+    projectName[elementNum].className += hide;
+    projectDesc[elementNum].className += hide;
+    projectPic[elementNum].className -= show;
+    elementNum -= 1 
+    if (elementNum<0) {
+        elementNum += projectPic.length;
     }
-    links[i].className -= hide;
-    projectPic[i].className += show;
-    projectName[i].className -= hide;
-    projectDesc[i].className -= hide;
-    projectName[i].className += showName;
-    projectDesc[i].className += showDesc;
+    links[elementNum].className -= hide;
+    projectPic[elementNum].className += show;
+    projectName[elementNum].className -= hide;
+    projectDesc[elementNum].className -= hide;
+    projectName[elementNum].className += showName;
+    projectDesc[elementNum].className += showDesc;
     
 }
+// console.log(skillsPanel.style.animation)
+// function skillsAccent () {
+//     console.log(skillsPanel.style.animation)
+//     if (skillsPanel.style.animation === "2s ease 0s 1 normal none running skills-accent") {
+//         // skillsPanel.style.removeProperty(animation);
+//         console.log(skillsPanel.style.animation)
+//         skillsPanel.style.animation = "0s ease 0s 1 normal none running skills-accent";
+//         console.log(skillsPanel.style.animation)
+//     } 
+//      skillsPanel.style.animation = "skills-accent 2s";
+//      console.log(skillsPanel.style.animation);
+//     // skillsPanel.style.backgroundColor = "red";
+// }
+
+
+// console.log(skillsPanel.style.animation)
 
 next.addEventListener("click", showNext);
+
+
+
 previous.addEventListener("click", showPrevious);
+
+
+if(document.getElementById("skills-button-main")){
+$("#skills-button-main").click(function() {
+   
+    
+    const el = $("#skills-panel").addClass("skills-animation");
+    setTimeout(function() {
+        el.removeClass("skills-animation");
+    }, 2000);
+});
+}
+
+
+if (document.getElementById("skills-button-projects")){ 
+    document.getElementById("skills-button-projects").addEventListener("click", function() {
+    
+    
+    window.location.href = "./index.html";
+    
+    if(document.getElementById("skills-button-main")){
+        $("#skills-button-main").click(function() {
+            const el = $("#skills-panel").addClass("skills-animation");
+            setTimeout(function() {
+                el.removeClass("skills-animation");
+            }, 2000);
+        })};
+    });
+    // document.getElementById(localStorage.getItem("mykey")).style.animation = "skills-accent 2s";
+   
+        
+        
+    
+};
+
+// skillsButton.addEventListener("click", skillsAccent);
 
 // projects[3].className -= show
 // projects[0].className += show
 
-
+// $(window).observe('domready', function () {
+//     $(location.hash).click();
+// });
 
 
 
@@ -109,7 +170,9 @@ previous.addEventListener("click", showPrevious);
 
 
 // FOR LATER
-
+// $("#skills-button-main").bind("webkitAnimationEnd mozAnimationEnd animationEnd", function(){
+//          $("#skills-panel").removeClass("skills-animation")  ;
+//      })
 
 
 
@@ -140,4 +203,4 @@ previous.addEventListener("click", showPrevious);
 
 //   document.getElementById("line-1-animate").style.backgroundColor = "green";
 
-//   document.getElementById("boxie").style.backgroundColor = "green";
+//   document.getElementById("boxie").style.backgroundColor = "green";}
